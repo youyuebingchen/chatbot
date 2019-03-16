ce61a12307c7eecdfcfb449abb878bee28df0f11
youyuebingchen <1625628484@qq.com>
2019-03-16 11:10:57 +0800

    update

--- rule_basedchatbot/upgrate_1_rule_based.py
+++ rule_basedchatbot/upgrate_1_rule_based.py
@@ -1,2 +1,19 @@
 from nltk import word_tokenize
-import ra
+import random
+greeting = ["hola","hello","hi","Hi","hey","hey!"]
+question = ["break","holiday","vacation","weekend"]
+responses  = ["It was nice! I went to paris","Sadly, I just stayed at home"]
+
+while True:
+    rand_greeting = random.choice(greeting)
+    rand_respon = random.choice(responses)
+    usrinput = input(">>>")
+    cleand_input = word_tokenize(usrinput)
+    if not set(cleand_input).isdisjoint(greeting):
+        print(rand_greeting)
+    elif not set(cleand_input).isdisjoint(question):
+        print(rand_respon)
+    elif usrinput == "bye":
+        break
+    else:
+        print("I don't know what you are saying")
